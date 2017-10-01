bc07d82f05a73a91c18bb6f00460a570c6e66aa0
Enreeco MacBookAir <enrico.murru@gmail.com>
2017-10-01 10:21:22 +0200

    initial commit

--- libs/constants.js
+++ libs/constants.js
@@ -8,9 +8,9 @@ window.$Constants = window.$Constants || (function(){
     return{
         MESSAGES: {
             GET_ORG_ID_BKG: 'BKG-GET-ORG-SID',
-            GET_ORG_ID_CNT: 'CNT-SET-ORG-SID',
         },
         API_LEVEL: '40.0',
         CLIENT_ID: '3MVG9HxRZv05HarSpKLUXc5SQ3BzfEXx6Z3RBWxt__OGSdojuqsg5TEoAqJgwkKguUoWdlYNMydnz6hHx3cJA',
+        //CLIENT_ID: '3MVG9HxRZv05HarSpKLUXc5SQ3Ee.lRGqW8jFPJNNewKd9NsucYwNuSh8De037ITuVaIxZqna6.WpRxxQkqTZ',
     };
 })();
